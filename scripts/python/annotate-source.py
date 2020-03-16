3bd98f4063c6f61a06c1fd4c4026227727271d8b
Takashi Kokubun <takashikkbn@gmail.com>
2020-03-16 01:41:54 -0700

    Implement initial EventProcessor

--- scripts/python/annotate-source.py
+++ scripts/python/annotate-source.py
@@ -1,6 +1,6 @@
+# coding: utf-8
 import os
 import sys
-import time
 
 sys.path.append(os.environ['PERF_EXEC_PATH'] + \
     '/scripts/python/Perf-Trace-Util/lib/Perf/Trace')
@@ -8,15 +8,50 @@ sys.path.append(os.environ['PERF_EXEC_PATH'] + \
 from perf_trace_context import *
 from Core import *
 
-def trace_begin():
-    global event_num
-    event_num = 0
+class EventProcessor:
+    def __init__(self):
+        self.total_events = 0
+        self.sources = set()
+        self.dsos = set()
 
-def process_event(param_dict):
-    global event_num
-    event_num += 1
-    print(param_dict['callchain'])
+    def process_event(self, attr, sample, comm, ev_name, raw_buf, callchain, dso=None, symbol=None):
+        sample = next((frame for frame in callchain if self.annotatable(frame)), None)
+        if sample:
+            self.process_sample(**sample)
+        self.total_events += 1
+
+    def process_sample(self, ip, dso, sym=None):
+        print((dso, ip))
+
+    def annotatable(self, sample):
+        # TODO: remove the hard coding
+        return sample.get('dso') and sample['dso'] not in ['[kernel.kallsyms]', '/lib/x86_64-linux-gnu/libc-2.27.so']
+
+# lineno => samples
+class Source:
+    def __init__(self, path):
+        self.path = path
+
+# address_range => (filename, lineno)
+class DynamicSharedObject:
+    def __init__(self, path):
+        self.path = path
+
+class SourceAnnotator:
+    def __init__(self, total_events):
+        self.total_events = total_events
+
+    def annotate(self, source):
+        pass
+
+def trace_begin():
+    global processor
+    processor = EventProcessor()
+
+def process_event(event):
+    processor.process_event(**event)
 
 def trace_end():
-    global event_num
-    print(event_num)
+    annotator = SourceAnnotator(processor.total_events)
+    for source in processor.sources:
+        annotator.annotate(source)
