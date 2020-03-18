7b59cf80a04c82f446f9a1dabe30366c72a8d515
Takashi Kokubun <takashikkbn@gmail.com>
2020-03-18 02:11:29 -0700

    Implement initial annotator

--- scripts/python/annotate-source.py
+++ scripts/python/annotate-source.py
@@ -24,11 +24,11 @@ class EventProcessor:
 
     def process_sample(self, ip, dso, sym=None):
         source, lineno = self.retrieve_dso(dso).source_lineno(ip)
-        print(source + ':' + str(lineno))
         self.retrieve_source(source).increment_samples(lineno)
 
     def retrieve_dso(self, dso):
         if dso not in self.dsos:
+            print('Reading ' + dso + '...')
             self.dsos[dso] = DynamicSharedObject(dso)
         return self.dsos[dso]
 
@@ -52,7 +52,7 @@ class DynamicSharedObject:
             else:
                 entry = re.split('\s+', line)
                 if len(entry) >= 3 and source and entry[0] == os.path.basename(source):
-                    entries.append((source, entry[1], int(entry[2], 0)))
+                    entries.append((source, int(entry[1]), int(entry[2], 0)))
 
         # List of `(source, lineno, address)` sorted by `address` for bisect
         self.entries = sorted(entries, key=lambda entry: entry[2])
@@ -93,11 +93,9 @@ class DynamicSharedObject:
             entry = self.entries[bisect.bisect_right(self.keys, address) - 1]
             return entry[0]
 
-# lineno => samples
 class Source:
     def __init__(self, path):
         self.path = path
-        self.last_lineno = 0 # TODO: implement
         self.lineno_samples = {}
 
     def increment_samples(self, lineno):
@@ -107,11 +105,52 @@ class Source:
         return self.lineno_samples.get(lineno, 0)
 
 class SourceAnnotator:
+    MIN_PERCENT = 0.10 # TODO: add an option to change this
+    SURROUND_LINES = 5
+
     def __init__(self, total_events):
         self.total_events = total_events
 
     def annotate(self, source):
-        pass # TODO: implement
+        lineno_rates = self.calc_and_filter_rates(source.lineno_samples)
+        if not lineno_rates:
+            return
+
+        with open(source.path, 'r') as f:
+            lines = f.readlines()
+        linenos = self.pick_linenos(lineno_rates.keys(), len(lines))
+
+        print('\nFrom: ' + source.path)
+        prev_lineno = None
+        for lineno in linenos:
+            if prev_lineno and lineno != prev_lineno + 1:
+                print('')
+            prev_lineno = lineno
+
+            if lineno in lineno_rates:
+                samples = source.lineno_samples[lineno]
+                print('%6d (%5.2f%%) |%6d | %s' % (samples, lineno_rates[lineno], lineno, lines[lineno - 1])),
+            else:
+                print('                |%6d | %s' % (lineno, lines[lineno - 1])),
+
+    def calc_and_filter_rates(self, lineno_samples):
+        rates = {}
+        for lineno, samples in lineno_samples.items():
+            rate = 100.0 * samples / self.total_events
+            if rate > self.MIN_PERCENT:
+                rates[lineno] = rate
+        return rates
+
+    def pick_linenos(self, rated_linenos, max_lineno):
+        linenos = []
+        min_lineno = 1
+        for rated_lineno in sorted(rated_linenos):
+            beg_lineno = max(min_lineno, rated_lineno - self.SURROUND_LINES)
+            end_lineno = min(rated_lineno + self.SURROUND_LINES, max_lineno)
+            for lineno in range(beg_lineno, end_lineno + 1):
+                linenos.append(lineno)
+            min_lineno = end_lineno + 1
+        return linenos
 
 
 def trace_begin():
@@ -124,6 +163,5 @@ def process_event(event):
 
 def trace_end():
     annotator = SourceAnnotator(processor.total_events)
-    for source in processor.sources:
+    for source in processor.sources.values():
         annotator.annotate(source)
-    print(processor.total_events) # TODO: remove
